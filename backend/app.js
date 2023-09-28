df2e225d40a48d51fc6bf8d2a826808f1a5287e0
Your Name <fred.postles@gmail.com>
2023-09-28 20:16:36 +0100

    Completed backend code with addition of fetching temperature data and finding correct temp to display based on current time

--- backend/app.js
+++ backend/app.js
@@ -27,9 +27,10 @@ fs.createReadStream("../data/gb.csv")
         console.log("CSV file successfully processed.");
     });
 
-// API Endpoint for temperature requests
+// GET Endpoint for temperature data
 app.get('/getTemperature', async (req, res) => {
   const cityName = req.query.city;
+  console.log("cityName in backend:", cityName);
 
    // if no cityName received, handle error
    if (!cityName) {
@@ -41,19 +42,64 @@ app.get('/getTemperature', async (req, res) => {
 
   // if no matching city, handle error
   if (!cityEntry || cityEntry === "undefined") {
+    console.log("City not found in CSV file in backend.");
     return res.status(404).json({ error: "City not found." });
 }
 
-// if city found
-return res.status(200).json({cityEntry})
+  // otherwise, get temperature data for that city
+  const temperatureData = await fetchTemperatureData(cityEntry);
 
-})
+  // process temp data and find closest temp to current time
+  const currentTemperature = findCurrentTemperature(temperatureData);
+
+// if city && temp data found
+return res.status(200).json({cityEntry, temperature: currentTemperature});
+});
 
 // find city in csv file
 const getCityData = (cityName) => {
   const result = cityData.find((entry) => entry.city.toLowerCase() === cityName.toLowerCase());
 // console.log("result of getCityData", result);
   return result;
+}
+
+// fetch temperature data from Open Meteo
+const fetchTemperatureData = async (cityEntry) => {
+ // grab lat and long from city data
+ const lat = cityEntry.lat;
+ const long = cityEntry.lng;
+
+ const {data} = await axios.get(`https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${long}&hourly=temperature_2m`)
+  
+ console.log("temperature data:", data);
+return data;
+}
+
+
+const findCurrentTemperature = (temperatureData) => {
+// Get the current time
+const currentTime = new Date();
+
+// Find the closest timestamp
+const timestamps = temperatureData.hourly.time;
+let closestTimestamp = timestamps[0]; // Initialize with the first timestamp
+let closestTimeDifference = Math.abs(new Date(closestTimestamp) - currentTime);
+
+for (const timestamp of timestamps) {
+  const timeDifference = Math.abs(new Date(timestamp) - currentTime);
+  if (timeDifference < closestTimeDifference) {
+      closestTimestamp = timestamp;
+      closestTimeDifference = timeDifference;
+  }
+}
+
+// Find the temperature for the closest timestamp
+const hourlyTempData = temperatureData.hourly.temperature_2m;
+const indexOfClosestTimestamp = timestamps.indexOf(closestTimestamp);
+const closestTemperature = hourlyTempData[indexOfClosestTimestamp];
+
+
+return closestTemperature;
 }
 
 app.listen(port, () => {
--- frontend/js/main.js
+++ frontend/js/main.js
@@ -8,29 +8,44 @@ document.addEventListener("DOMContentLoaded", () => {
         // get city name from input
         const cityName = cityInput.value.trim();
 
-        // if cityname blank, display error message, otherwise fetch data
+        // if cityName is null or blank, display error message
+        // otherwise fetch data
         if(!cityName || cityName === ""){
-            output.innerHTML = `<p>Please enter a city name.</p>`
+            output.innerHTML = `<p>Please enter a city name.</p>`;
         } else {
             try {
                 axios.get(`http://localhost:3000/getTemperature?city=${cityName}`)
                 .then((response)=> {
-                    console.log("response:", response)
+                    console.log("response.data:", response.data);
+                    console.log("response status:", response.status);
+
                     const data = response.data;
-                    console.log("data:", data)
-                     if (data.temperature) {
-                        output.innerHTML = `<p>The current temperature in ${cityName} is ${data?.temperature}.</p>`
-                    } else {
-                        console.log("no temp data")
-                        output.innerHTML = `<p>The current temperature in ${data.cityEntry.city} is unavailable. Please try again later.</p>`
-                        return;
-                    }
-                });
+
+                    if(data.error === "City not found."){
+                        output.innerHTML = `<p>Data for ${cityName} is unavailable. Please try another city.</p>`;
+                    } else if(response.status === 200){
+                        // city found but no temp data available
+                        if(data.cityEntry && !data.temperature) {
+                            console.log("Temperature data not available for this city. Check spelling or try another city.");
+                            output.innerHTML = `<p>Sorry, temperature data is not currently available for ${cityName}. Please try another city.</p>`;
+                        } else if(data.temperature) {
+                            // temp data found for cityName
+                            output.innerHTML = `<p>The current temperature in ${data.cityEntry.city} is ${data.temperature}ºC.</p>`;
+                        } else {
+                            console.log("City not found - inside 200 status code response");
+                            output.innerHTML = `<p>Data for ${cityName} is unavailable. Please try again later.</p>`;
+                        }
+                    } 
+                
+                    })
+                    .catch((error) => {
+                        console.error("Axios error:", error);
+                        output.innerHTML = `<p>An error occurred while fetching data for ${cityName}. Please try again later.</p>`;
+                    });
             } catch(error) {
+                console.log("here");
                 console.error("An error occured fetching data:", error);
             }
-            output.innerHTML = `<p>Sorry, that city was not found. Please try another</p>`
-
-        }
+        } 
     });
 });
